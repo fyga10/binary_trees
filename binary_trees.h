a36b5a5a97bdc874062b1215026dd25068be5189
fyga10 <frankgiraldo62@outlook.com>
2022-03-23 13:15:29 -0500

    order prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -19,8 +19,8 @@ struct binary_tree_s *left;
 struct binary_tree_s *right;
 };
 
-void binary_tree_print(const binary_tree_t *tree);
 typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *tree);
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
