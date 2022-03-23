ec95a7e19858008cf3102bb6ad6f149b4e129249
fyga10 <frankgiraldo62@outlook.com>
2022-03-23 13:04:13 -0500

    prototypes

---
+++ binary_trees.h
@@ -0,0 +1,34 @@
+#ifndef BINARY_TREES
+#define BINARY_TREES
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: The parent node
+ * @left: The left child node
+ * @right: The right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *left;
+    struct binary_tree_s *parent;
+	struct binary_tree_s *right;
+};
+
+void binary_tree_print(const binary_tree_t *tree);
+typedef struct binary_tree_s binary_tree_t;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+#endif
